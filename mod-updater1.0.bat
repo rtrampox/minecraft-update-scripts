f7c334f61e616c2f980211e640ef6ee1efb33394
rtrampox <isaquewrodrigues@icloud.com>
2024-02-07 17:22:00 -0300

    Added Crayfish's Furniture Mod

--- mod-updater1.0.bat
+++ mod-updater1.0.bat
@@ -2,11 +2,14 @@
 ECHO Aguarde, realizando o download dos Mods.
 curl --remote-name https://cdn.rtrampox.cloud/industrial-foregoing.jar
 curl --remote-name https://cdn.rtrampox.cloud/titanium.jar
+curl --remote-name https://cdn.rtrampox.cloud/crayfishfurniture.jar
 ECHO Mais um momento, estamos instalando os mods!
 move  "industrial-foregoing.jar"  "%USERPROFILE%\AppData\Roaming\.minecraft\mods"
 move  "titanium.jar"  "%USERPROFILE%\AppData\Roaming\.minecraft\mods"
+move "crayfishfurniture.jar" "%USERPROFILE%\AppData\Roaming\.minecraft\mods"
 del "industrial-foregoing.jar"
 del "titanium.jar"
+del "crayfishfurniture.jar"
 ECHO Mods Atualizados com sucesso!
 cls
 pause
