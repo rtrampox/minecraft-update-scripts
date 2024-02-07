4761377528a552c3de9b4e13b575a13ceb3a9590
rtrampox <isaquewrodrigues@icloud.com>
2024-02-07 17:52:53 -0300

    Added MrCrayfish's Furtiniture Mod dependecies.

--- mod-updater.bat
+++ mod-updater.bat
@@ -3,13 +3,16 @@ ECHO Aguarde, realizando o download dos Mods.
 curl --remote-name https://cdn.rtrampox.cloud/industrial-foregoing.jar
 curl --remote-name https://cdn.rtrampox.cloud/titanium.jar
 curl --remote-name https://cdn.rtrampox.cloud/crayfishfurniture.jar
+curl --remote-name https://cdn.rtrampox.cloud/Patchouli.jar
 ECHO Mais um momento, estamos instalando os mods!
 move  "industrial-foregoing.jar"  "%USERPROFILE%\AppData\Roaming\.minecraft\mods"
 move  "titanium.jar"  "%USERPROFILE%\AppData\Roaming\.minecraft\mods"
 move "crayfishfurniture.jar" "%USERPROFILE%\AppData\Roaming\.minecraft\mods"
+move "Patchouli.jar" "%USERPROFILE%\AppData\Roaming\.minecraft\mods"
 del "industrial-foregoing.jar"
 del "titanium.jar"
 del "crayfishfurniture.jar"
+del "Patchouli.jar"
 ECHO Mods Atualizados com sucesso!
 cls
 pause
